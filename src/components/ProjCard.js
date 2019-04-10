f9609655e8c3d7294684841c0b75e3d2618ce113
Keegan Lawley <keeganlk.7@gmail.com>
2019-04-10 18:35:09 -0500

    image prop

--- src/components/ProjCard.js
+++ src/components/ProjCard.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react'
 
 class ProjCard extends Component {
   render() {
+    const images = this.props.tech.map(image => (
+      <img src={image} width="40px" height="40px" />
+    ));
     return (
       <div className="proj-card">
         <h2>{this.props.name}</h2>
+        <h4 className="tagname">{this.props.tagline}</h4>
         <h4>
           Link:
           <a
@@ -16,6 +20,7 @@ class ProjCard extends Component {
           </a>
         </h4>
         <p>{this.props.description}</p>
+        {images}
       </div>
     );
   }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,6 +17,7 @@ export default class componentName extends Component {
         <ProjCard
           name="Discover Weekly Dump"
           link="https://github.com/keeganl/Discover-Weekly-Dump"
+          tagline="An application that lets you view the 3D models on your computer in gallery format. Built with Electron"
           description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa
             veniam dolores deleniti, omnis expedita voluptas harum numquam
             minus ipsa, delectus repudiandae. Asperiores pariatur deserunt
@@ -25,6 +26,7 @@ export default class componentName extends Component {
         <ProjCard
           name="Orbit"
           link="https://github.com/keeganl/orbit"
+          tagline="An application that lets you view the 3D models on your computer in gallery format. Built with Electron."
           description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa
             veniam dolores deleniti, omnis expedita voluptas harum numquam
             minus ipsa, delectus repudiandae. Asperiores pariatur deserunt
@@ -33,10 +35,8 @@ export default class componentName extends Component {
         <ProjCard
           name="Jot"
           link="https://github.com/keeganl/Jot."
-          description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa
-            veniam dolores deleniti, omnis expedita voluptas harum numquam
-            minus ipsa, delectus repudiandae. Asperiores pariatur deserunt
-            fuga optio unde corporis magni ut?"
+          tagline="A whiteboard that lives in the menubar."
+          description=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla numquam, temporibus distinctio laudantium, quo quod autem dolorum doloremque accusantium, animi dolore sequi voluptatibus quisquam. Placeat fugit libero eligendi quisquam nam."
         />
       </div>
     );
