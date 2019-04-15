322ffb9b5a9941fe9299cc609b8d9eaac17fe52b
Keegan Lawley <keeganlk.7@gmail.com>
2019-04-14 23:33:09 -0500

    added some info

--- src/components/About.js
+++ src/components/About.js
@@ -23,8 +23,11 @@ class About extends Component {
               }}
             />
             <p>
-              I am a JavaScript developer who enjoys design and photography.
-          </p>
+              I am a JavaScript developer who enjoys design and photography. Currently I am interning at Jellyfish Health working on a building patient experience software with AngularJS. In my free time I like to write React applications
+            </p>
+            <h3>Interests</h3>
+            <p>GraphQL</p>
+            <p>Docker</p>
           </div>
         </div>
       </React.Fragment>
